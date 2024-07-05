75fedcbe4707ed4ec497833a4628f2fff5beadf4
c-santos <jcms121899@gmail.com>
2024-07-05 22:09:07 +0800

    feat(http): added json response base handler

--- hello.go
+++ hello.go
@@ -1,24 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
+type Response struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+}
+
+func baseHandler(w http.ResponseWriter, r *http.Request, response Response) {
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(jsonResponse)
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Message: "Hello, you've called me!",
+		Status:  200,
+	}
+
+	baseHandler(w, r, response)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	// response := Response{
+	// 	Message: "Hello, %s\n", r.PathValue("name"),
+	// 	Status: 200,
+	// }
+
+	// baseHandler(w, r, response)
+}
+
 func main() {
 
-    helloHandler := func (w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "Hello, you've called me!")
-    }
+    port := ":8000"
 
-    userHandler := func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %s\n", r.PathValue("name"))
-    }
+	http.HandleFunc("/", healthCheckHandler)
+	// http.HandleFunc("/hello/{name}", helloHandler)
 
-    http.HandleFunc("/", helloHandler)
-    http.HandleFunc("/hello/{name}", userHandler)
-
-    log.Fatal(http.ListenAndServe(":8000", nil))
+    log.Printf("Listening on %s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
