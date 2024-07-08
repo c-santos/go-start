d3f2075117eff7c7224ac345f7f859bdf3b88723
c-santos <jcms121899@gmail.com>
2024-07-08 18:35:02 +0800

    chore(handler): added passing of status code to respond

--- pkg/handlers/respond.go
+++ pkg/handlers/respond.go
@@ -5,8 +5,9 @@ import (
 	"net/http"
 )
 
-func Respond(w http.ResponseWriter, response any) {
+func Respond(w http.ResponseWriter, response any, code int) {
     w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
     err := json.NewEncoder(w).Encode(response)
     if err != nil {
         http.Error(w, err.Error(), 500)
